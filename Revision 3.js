1efe87fc794ae13513e4de56fe9d82f554539bad
cricketfevr <48909964+cricketfevr@users.noreply.github.com>
2019-03-26 17:02:07 +0530

    Revision 3
    Testing grant items to user

---
+++ Revision 3.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+handlers.GrantItemsToPlayer = function (args, context) {
+    
+    var grantPlayersRequest = server.GrantItemsToUser({
+        PlayFabId: currentPlayerId,
+        CatalogVersion: "1",
+        ItemIds: ["Dhoni", "RohitSharma"]
+    });
+
+    server.GrantItemsToUser(grantPlayersRequest);
+
+    var resultitems = grantPlayersRequest.Item
+
+    var message = "Hello " + currentPlayerId + "!";
+
+}
