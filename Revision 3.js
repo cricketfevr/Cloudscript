5bd26f4baf6c68462ead18bb625898b734d97e84
cricketfevr <48909964+cricketfevr@users.noreply.github.com>
2019-03-26 17:21:35 +0530

    Update Revision 3.js

--- Revision 3.js
+++ Revision 3.js
@@ -4,7 +4,7 @@ handlers.GrantItemsToPlayer = function (args, context) {
     var grantPlayersRequest = server.GrantItemsToUser({
         PlayFabId: currentPlayerId,
         CatalogVersion: "1",
-        ItemIds: [args.player[0], args.player[1]]
+        ItemIds: [args.player[0]]
     });
 
     server.GrantItemsToUser(grantPlayersRequest);
